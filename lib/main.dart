87880b85a454625e35011f8c21aa8cef2e591334
Ere-yiga <erehdagr8@gmail.com>
2026-02-23 19:16:06 +0100

    worked on my listview section

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,92 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  
+    runApp(FoodMenu());
+}
+
+class FoodMenu extends StatelessWidget{
+    const FoodMenu({super.key});
+
+    @override
+    Widget build(BuildContext context){
+        return (
+            MaterialApp(
+                debugShowCheckedModeBanner: false,
+                home: Scaffold(
+                    appBar: AppBar(
+                        title: Column(
+                            children: [
+                                Text("Menu"),
+                                Text("Inie food menu",
+                                style: TextStyle(fontSize: 17, )),
+                            ],
+                        ),
+                        backgroundColor: Colors.blue,
+                        centerTitle: true,   
+                    ),
+                    body: Column(
+                        children: [
+                            Center(
+                               child: Text("Category", 
+                               //textAlign:TextAlign.center,
+                               style: TextStyle(fontSize: 30)),
+                            ),
+
+                            Container(
+                                height: 150,    
+                                child: ListView(
+                                    scrollDirection: Axis.horizontal,
+                                    children: [
+                                        Container(
+                                            height: 100,
+                                            width:200,
+                                            margin: EdgeInsets.all(8),
+                                            color: Colors.grey,
+                                            child: Center(child: Text("Burger"))
+                                        ),
+                                        Container(
+                                            height: 100,
+                                            width:200,
+                                            margin: EdgeInsets.all(8),
+                                            color: Colors.red,
+                                            child: Center(child: Text("MatPie"))
+                                        ),
+                                        Container(
+                                            height: 100,
+                                            width:200,
+                                            margin: EdgeInsets.all(8),
+                                            color: Colors.blue,
+                                            child: Center(child: Text("Pizza"))
+                                        ),
+                                        Container(
+                                            height: 100,
+                                            width:200,
+                                            margin: EdgeInsets.all(8),
+                                            color: Colors.purple,
+                                            child: Center(child: Text("Sausage"))
+                                        ),
+                                        Container(
+                                            height: 100,
+                                            width:200,
+                                            margin: EdgeInsets.all(8),
+                                            color: Colors.orange,
+                                            child: Center(child: Text("Spagheti"))
+                                        ),
+                                        Container(
+                                            height: 100,
+                                            width:200,
+                                            margin: EdgeInsets.all(8),
+                                            color: Colors.green,
+                                            child: Center(child: Text("read"))
+                                        ),
+                                    ],
+                                ),
+                            )
+                        ]
+                    )
+                ),
+                
+            )
+        );
+    }
 }
