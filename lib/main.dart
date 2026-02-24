d0c38b37e4f3452aa7f92074eca66a9115694da7
Ere-yiga <erehdagr8@gmail.com>
2026-02-24 16:18:21 +0100

    successfully implemented navigation across different screens

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:newp/Pagez/first_page.dart';
 
 void main(){
-    runApp(MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget{
-    const MyApp ({super.key});
+  const MyApp ({super.key});
 
-    @override
-    Widget build(BuildContext context){
-        return MaterialApp(
-            home: FirstPage(), 
-        );
-    }
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstPage(), 
+    );
+  }
 }
 
