a35b1d0cd62e7afbd8602df40e956e84dd256f45
Ere-yiga <erehdagr8@gmail.com>
2026-03-01 00:44:42 +0100

    worked more on the drawer to link to another page

--- lib/Pagez/first_page.dart
+++ lib/Pagez/first_page.dart
@@ -8,14 +8,27 @@ class FirstPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Page One")),
-      drawer: Drawer(),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(child: Text("Ugonna Menu")),
+            ListTile(
+              title: Text("Page two"),
+              onTap: (){
+                Navigator.pushNamed(
+                context, '/SecondPage');
+              }
+            )
+          ],
+        )
+      ),
       body: Center(
         child: ElevatedButton(
           onPressed: (){
           Navigator.pushNamed(context, '/SecondPage');},
           child: Text("Next page"),
         ),
-      ), 
+      ),
     );
   }
 }
