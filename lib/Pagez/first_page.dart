04597f778da48247854563fbe00e87b0f8b18c38
Ere-yiga <erehdagr8@gmail.com>
2026-02-28 23:39:11 +0100

    added drawer...(for side navigation

--- lib/Pagez/first_page.dart
+++ lib/Pagez/first_page.dart
@@ -8,6 +8,7 @@ class FirstPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Page One")),
+      drawer: Drawer(),
       body: Center(
         child: ElevatedButton(
           onPressed: (){
